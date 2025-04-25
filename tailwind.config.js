16e0666fbe280049319eb8dbc5f187f1b534976a
Nicholas Le <nicholasl.le224@gmail.com>
2025-04-24 19:26:40 -0700

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('taos/plugin')
+  ],
   variants: {
     extend: {
         display: ["group-hover"],
     },
-},
+  },
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./**/*.{html,js}'],
+  },
 }
